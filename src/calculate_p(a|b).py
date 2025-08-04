179ea50f3733ad5dc9393a7262f1c1f174f73a64
Dylan Hilburn <dylan.hilburn@bruins.belmont.edu>
2025-08-04 13:29:11 -0500

    added get_sample_size() still working version

--- src/calculate_p(a|b).py
+++ src/calculate_p(a|b).py
@@ -4,10 +4,24 @@ Calculates the probability of event A given event B using Bayes' theorem.
 
 a = [1, 2, 3, 4, 5]
 b = [4, 5, 6, 7, 8, 9]
-sample_size = 9
+s = 9
 
+def get_sample_size(a, b):
 
-def calculate_p_a_given_b(a, b, sample_size):
+    max_sample_size = max(len(a), len(b))
+    sample_size = 0
+
+    for i in len(max_sample_size):
+        if a(i) == b(i):
+            continue
+        else:
+            sample_size+=1
+
+    return sample_size
+
+print(get_sample_size)
+
+def calculate_p_a_given_b(a, b, s):
     """
     Calculate P(A|B) using Bayes' theorem.
     
@@ -15,11 +29,11 @@ def calculate_p_a_given_b(a, b, sample_size):
     
     For simplicity, we assume uniform probabilities for events A and B.
     """
-    p_a = len(a)/sample_size  # P(A)
-    p_b = len(b)/sample_size # P(B)
+    p_a = len(a)/s  # P(A)
+    p_b = len(b)/s # P(B)
     p_b_given_a = 2/5
 
     p_a_given_b = (p_b_given_a * p_a) / p_b
     return round(p_a_given_b, 2)
 
-print(f"P(A|B) = {calculate_p_a_given_b(a, b, sample_size)}")
+print(f"P(A|B) = {calculate_p_a_given_b(a, b, s)}")
